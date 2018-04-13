3dfa462f62d0d06e2d92fe6b8b741a8be23b0629
Johan Sutrisno <johansutrisno30@gmail.com>
2018-04-13 22:08:22 +0700

    Add files via upload

---
+++ BaliTour/history.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HISTORY || BALI TOUR</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/slider.css">
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,500' rel='stylesheet'>
+	<link href="https://fonts.googleapis.com/css?family=PT+Sans|Slabo+27px" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <style type="text/css">
+        .parallax {
+            margin-top: 100px;
+            background-image: url("img/ngaben.jpg");
+            min-height: 630px; 
+
+            background-attachment: fixed;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+        .pembeli {
+            margin-top: 200
+        }
+    </style>
+</head>
+<body>
+	<div class="header">
+        <div class="navbar">
+            <div class="navbartop ul">
+                <div class="kiri">
+                    <ul>
+                        <li><a href="#"><b>IND</b> | ENG</a></li>
+                        <li>+8801711223344</li>
+                        <li><a href="#">haloadmin@bali.com</a></li>
+                    </ul>
+                </div>
+                <div class="kanan">
+                    <ul>
+                        <li><a href="#">Account</a></li>
+                        <li><a href="#">Wishlist</a></li>
+                        <li><a href="form-login.html">Register/Login</a></li>
+                        <li>
+                            <form>
+                                <input type="text" name="search" placeholder="Search..">
+                            </form>
+                        </li>
+                            
+                    </ul>
+                </div>
+            </div>
+            <div class="navbarbot"> 
+                <div class="logo">
+                    <a href="index.html"><b>BALI</b> TOUR</a>
+                </div>
+
+                <div class="main">
+                    <ul>
+                        <li><a href="index.html">HOME</a></li>
+                        <li><a href="whattodo.html">WHAT TO DO</a></li>
+                        <li><a href="history.html"><b>HISTORY</b></a></li>
+                        <li><a href="gallery.html">GALLERY</a></li>
+                        <li><a href="blog.html">BLOG</a></li>
+                        <li><a href="about.html">ABOUT US</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="parallax"></div>
+        <div class="judul" style="text-align: center">
+            <br>HISTORY
+        </div>
+
+            <div class="row">
+        <div class="col-12 col-m-12">
+            <div class="col-1"></div>
+            <div class="col-6 col-m-12 imgatas">
+                <img src="img/gal1.jpg" style="width : 100%;" >
+            </div>
+            <div class="col-4 col-m-12 isi">  
+                <div class="judul">Bali itu unik</div>
+                <p>Tidak ada tempat lain yang seperti Bali. Pulau Dewata menawarkan banyak hal, pantai dan gelombang yang cocok untuk surfing, keadaan alam yang indah untuk dikunjungi dan dijelajahi, upacara keagamaan, dan para seniman berbakat. Ada berbagai macam akomodasi, restoran, spa dan tempat belanja - semua dalam jarak yang dekat dan memiliki harga yang sangat pantas. Anda dapat memilih penginapan rumah yang dimiliki oleh keluarga Bali, resort pantai bintang 5 nan mewah atau vila pribadi dengan kolam renang.</p>
+                
+                <p>Tidak hanya itu, Bali menawarkan jauh lebih banyak. Ada getaran khusus, esensi, atau sesuatu yang sulit digambarkan, yang telah menyentuh dan menginspirasi pengunjung dari seluruh dunia sejak beberapa dekade. Ini ada hubungannya dengan Bali sendiri dan karakter penduduknya yang ramah.</p>
+                Welcome to Bali, welcome to <a href="incex.html"><b>balitour.com</b></a>
+            </div>
+            <div class="col-1"></div>
+        </div>
+
+    </div>
+
+        <div class="col-12 col-m-12 subscribe">
+        <div class="col-1"></div>
+        <div class="col-6" style="margin-top: 1%;">SIGN UP FOR NEWSLETTER</div>
+        <div class="col-5">
+            <form>
+                <input type="text" id="fname" name="fname" placeholder="Email Address">
+                <input type="button" value="SUBSCRIBE">
+            </form>
+        </div>
+    </div>
+    <div class="col-12 col-m-12 info">
+        <div class="col-2"></div>
+        <div class="col-2 col-m-6">
+            Bali Tour
+            <hr>
+            <div class="alamat">
+                <b>Address :</b> Jalan D.I.Panjaitan nomer 128, Denpasar, Bali.
+            </div>
+            <div class="alamat">
+                <b>Phone : </b> +8801711223344
+            </div>
+            <div class="alamat">
+                <b>Email : </b> haloadmin@bali.com
+            </div>
+        </div>
+        <div class="col-2 col-m-6">
+            My Account
+            <hr>
+            <div class="alamat">
+                <a href="">My Account</a>
+            </div>
+            <div class="alamat">
+                <a href="">My Wishlist</a>
+            </div>
+            <div class="alamat">
+                <a href="">My Shopping Cart</a>
+            </div>
+            <div class="alamat">
+                <a href="">My Credit</a>
+            </div>
+        </div>
+        <div class="col-2 col-m-6">
+            About us
+            <hr>
+            <div class="alamat">
+                <a href="">Transportation</a>
+            </div>
+            <div class="alamat">
+                <a href="">Hotel</a>
+            </div>
+            <div class="alamat">
+                <a href="">Interested place</a>
+            </div>
+            <div class="alamat">
+                <a href="">Tour Guide</a>
+            </div>
+        </div>
+        <div class="col-2 col-m-6">
+            Information
+            <hr>
+            <div class="alamat">
+                <a href="">About our service</a>
+            </div>
+            <div class="alamat">
+                <a href="">Secure Booking</a>
+            </div>
+            <div class="alamat">
+                <a href="">Delivery Information</a>
+            </div>
+            <div class="alamat">
+                <a href="">Privacy Policy</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-12 col-m-12 footer">
+        <div class="col-1"></div>
+        <div class="col-7 kiri">Copyright 2016 FlyHigh All rights reserved.<br>
+        <a href=""><img src="img/icon/facebook.png"></a>
+            <a href=""><img src="img/icon/instagram.png"></a>
+            <a href=""><img src="img/icon/twitter.png"></a>
+            <a href=""><img src="img/icon/youtube.png"></a>
+        </div>
+        <div class="col-4 kanan">
+            <img src="img/footer/paypal-1.png">
+            <img src="img/footer/paypal-2.png">
+            <img src="img/footer/paypal-3.png">
+            <img src="img/footer/paypal-4.png">
+            <img src="img/footer/paypal-5.png">
+        </div>
+	</div>
+
+    <?php 
+        for ($i=0; $i < ; $i++) { 
+            for ($i=0; $i < ; $i++) { 
+                # code...
+            }
+        }
+     ?>
+
+    <div class="col-12 col-m-12 footer">
+        <div class="col-1"></div>
+        <div class="col-7 kiri">
+            Copyright 2016 FlyHigh Team All Right Reserved.<br>
+            <a href=""><img src="img/icon/facebook.png"></a>
+        </div>
+    </div>
+</body>
+</html>
